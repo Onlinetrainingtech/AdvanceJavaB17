ef1572946990b6ee5366c7d784bcea97374b8dfc
OnlineTrainer <mohamedtechdata@gmail.com>
2023-02-18 08:16:40 +0530

    18-02-23

---
+++ Spring-Demo-One/src/com/xyz/springdemo/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.xyz.springdemo;
+
+public class BaseballCoach implements Coach
+{
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Spend 30 mins BaseballCoach";
+	}
+   
+}
